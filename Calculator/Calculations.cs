3a0263ba5b689bd8e07c4a4b60181f57ecdcf4ef
MAC <mac@MacBook-Air-MAC.local>
2020-07-26 09:46:42 +0300

    first commit

---
+++ Calculator/Calculations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Calculator
+{
+    public class Calculations
+
+    {
+
+        public int firstnumber;
+        public int secondnumber;
+       
+
+
+        public Calculations(int firstnumber, int secondnumber)
+        {
+            this.firstnumber = firstnumber;
+            this.secondnumber = secondnumber;
+
+        }
+
+        public int Plus(int firstnumber, int secondnumber)
+        {
+            return firstnumber + secondnumber;
+           
+        }
+
+        public int Minus(int firstnumber, int secondnumber)
+        {
+            return firstnumber - secondnumber;
+
+        }
+
+        public int Umnozhit(int firstnumber, int secondnumber)
+        {
+            return firstnumber * secondnumber;
+
+        }
+
+        public int Delit(int firstnumber, int secondnumber)
+        {
+            return firstnumber / secondnumber;
+
+        }
+
+        
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Please fill in first number");
+            int firstnumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please fill in operation");
+            char operation = char.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please fill in second number");
+            int secondnumber = int.Parse(Console.ReadLine());
+
+
+            Calculations a = new Calculations(firstnumber, secondnumber);
+
+         
+            switch (operation)
+            {
+
+                case '+':
+                    int sum = a.Plus(firstnumber, secondnumber);
+                    Console.WriteLine(sum);
+                    break;
+
+                case '-':
+                    int min = a.Minus(firstnumber, secondnumber);
+                    Console.WriteLine($"Answer is {min}");
+                    break;
+
+                case '*':
+                    int umn = a.Umnozhit(firstnumber, secondnumber);
+                    Console.WriteLine($"Answer is {umn}");
+                    break;
+
+                case '/':
+                    int del = a.Delit(firstnumber, secondnumber);
+                    Console.WriteLine($"Answer is {del}");
+                    break;
+
+                
+
+
+            }
+
+            
+
+
+
+        }
+    }
+}
